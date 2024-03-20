b3548a877cdf6d9cb79d70805d8f426d9ee80266
Swapneshh <sandhya@gmail.com>
2024-03-20 19:26:09 +0530

    new rps

--- website/Dashboard/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "explore_india";
-$conn = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
-mysql_select_db($mysql_database, $conn) or die("Could not select database");
-?>
--- website/Dashboard/funn.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getid(){
-    $query=mysql_query("SELECT COUNT(id) AS `cnt` FROM places_info");
-    $row=mysql_fetch_array($query);
-    return $row;
-}
-echo getid();
-?>
